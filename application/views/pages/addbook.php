2b7861fa79409369c1d735aaf5b714264434433a
Daniel Cheng Yuk-Pong <git@sdiz.net>
2010-10-14 22:23:13 +0800

    more server

--- application/views/pages/addbook.php
+++ application/views/pages/addbook.php
@@ -1,140 +1,163 @@
 <div id="addbookformwrap">
-<div style="width:20em; height:30ex; border: 1px solid black; float: right;"></div>
-<form id="addbookform" action="#">
-<fieldset>
-<legend>This is my form</legend>
-<ul>
-<li><label for="cat">Category</label> <select name="cat" id="cat">
-  <option value="">Please Select...</option>
-  <option value="A">A - Bible OT/NT</option>
-  <option value="B">B - Bible OT</option>
-  <option value="C">C - Bible NT</option>
-</select>
-<li><label for="isbn">ISBN</label> <input type="text" id="isbn" name="isbn" /></li>
-<li><label for="title">Book Title</label> <input type="text" id="title" name="title" /></li>
-<li><label for="author">Author</label> <input type="text" id="author" name="author" /></li>
-<li><label for="publisher">Publisher</label> <input type="text" id="publisher" name="publisher" /></li>
-<li><label for="publishDate">Publish Date</label> <input type="text" id="publishDate" name="publishDate" /></li>
-<li><label for="edition">Edition</label> <input type="text" id="edition" name="edition" /></li>
-<li><label for="quantity">Quantity</label> <input type="text" id="quantity" name="quantity" /></li>
-</ul>
-<fieldset>
-</form>
-</div>
-<?php echo HTML::script("static/js/jquery.validate.js"); ?>
-<script type="text/javascript" charset="utf-8"> 
-var marc;
+	<form id="addbookform" action="#">
+		<fieldset>
+			<legend>This is my form</legend>
+			<ul>
+				<li><label for="cat">Category</label> <select name="cat" id="cat">
+					<option value="">Please Select...</option>
+					<option value="A">A - Bible OT/NT</option>
+					<option value="B">B - Bible OT</option>
+					<option value="C">C - Bible NT</option>
+				</select>
+				<li><label for="isbn">ISBN</label> <input type="text" id="isbn" name="isbn" /></li>
+				<li><label for="title">Book Title</label> <input type="text" id="title" name="title" /></li>
+				<li><label for="author">Author</label> <input type="text" id="author" name="author" /></li>
+				<li><label for="publisher">Publisher</label> <input type="text" id="publisher" name="publisher" /></li>
+				<li><label for="publishDate">Publish Date</label> <input type="text" id="publishDate" name="publishDate" /></li>
+				<li><label for="edition">Edition</label> <input type="text" id="edition" name="edition" /></li>
+				<li><label for="quantity">Quantity</label> <input type="text" id="quantity" name="quantity" /></li>
+			</ul>
+			<fieldset>
+			</form>
+		</div>
+		<?php echo HTML::script("static/js/jquery.validate.js"); ?>
+		<?php echo HTML::script("static/js/jquery.dump.js"); ?>
+		<script type="text/javascript" charset="utf-8"> 
+			var marc;
+			var libraries = [
+			'library.cuhk.edu.hk',	// CU
+			'158.182.30.1',		// BU
+			'edlis.ied.edu.hk',	// IEd
+			'lnclib.ln.edu.hk',	// LU
+			'library.hku.hk',	// HKU
+			'lib.cityu.edu.hk',	// City
+			'library.polyu.edu.hk',	// PolyU
+			// DEAD	'nbinet.ncl.edu.tw',	// Taiwan Cent Lib
+			'ustlib.ust.hk',	// UST
+			'library.dts.edu',	// Dallas Theological Seminary (2200/unicon)
+			'fulleripac.fuller.edu',	// Fuller Theological Seminary (210/Horizon)
+			'library.ggbts.edu',		// Golden Gate Baptist Theological Seminary (7090/Voyager)
+			'library.bu.edu'	// Boston U
+			];
+			$(document).ready(function() {
+			$( "#publishDate" ).datepicker({ dateFormat: 'yy-mm-dd', autoSize: true, showOn: 'button' });
+				$.validator.addMethod("isbn", function (isbn, elem) {
+					if (validateISBN(isbn, elem)) {
+					marc = {"publishDate":[],"edition":[],"author":[],"title":[],"publisher":[]};
+						$.each(libraries, function(idx,val) {
+							$.ajax({
+								url: '<?php echo URL::base() ?>json/z3950/' + val + '/' + $(elem).val(),
+								dataType: 'json',
+								success: function(data) {
+									$.each(data, function(idx2, val2) {
+										$.each(val2, function(idx3, val3) {
+											marc[idx2].push(val3);
+										});
+										marc[idx2].sort();
+										marc[idx2] = $.unique(marc[idx2]);
+									});
+								}
+							});
+						});
 
-$(document).ready(function() {
-	$( "#publishDate" ).datepicker({ dateFormat: 'yy-mm-dd', autoSize: true, showOn: 'button' });
-	$.validator.addMethod("isbn", function (isbn, elem) {
-		if (validateISBN(isbn, elem)) {
-			marc = { title : [] };
-			$.ajax({
-				url: '<?php echo URL::base() ?>json/z3950/library.cuhk.edu.hk/' + $(elem).val(),
-				dataType: 'json',
-				success: function(data) {
-					marc = data;
-				}
-			});
-				
-			return true;
-		} else {
-			return false;
-		}
-	}, "error");
-	var validator = $("#addbookform").validate({
-		onblur: true,
-		onchange: true,
-		rules: {
-			cat: "required",
-			isbn: "isbn",
-			title: "required"
-		},
-		messages: {
-			cat: { required: "meow" },
-			isbn: { isbn: "Invalid ISBN" }
-		},
-		errorPlacement: function(error, element) {
-			error.appendTo(element.parent());
-		},
-		success: function(label) {
-			// set &nbsp; as text for IE
-			label.html("&nbsp; $$").addClass("checked");
-		}
-	});
+						return true;
+						} else {
+						return false;
+						}
+						}, "error");
+						var validator = $("#addbookform").validate({
+						onblur: true,
+						onchange: true,
+						rules: {
+						cat: "required",
+						isbn: "isbn",
+						title: "required"
+						},
+						messages: {
+						cat: { required: "meow" },
+						isbn: { isbn: "Invalid ISBN" }
+						},
+						errorPlacement: function(error, element) {
+						error.appendTo(element.parent());
+						},
+						success: function(label) {
+						// set &nbsp; as text for IE
+						label.html("&nbsp; $$").addClass("checked");
+						}
+						});
 
 
-	$('#title, #author, #publisher, #publishDate, #edition').autocomplete({
-		source: function (search, callback) {
-			callback(marc[this.element.attr('id')]);
-		},
-		minLength: 0,
-		delay: 100
-	});
-});
+						$('#title, #author, #publisher, #publishDate, #edition').autocomplete({
+						source: function (search, callback) {
+						if (marc && marc[this.element.attr('id')])
+						callback(marc[this.element.attr('id')]);
+						},
+						minLength: 0,
+						delay: 100
+						});
+						});
 
-// This functions takes a string containing
-// an ISBN (ISBN-10 or ISBN-13) and returns
-// true if it's valid or false if it's invalid.
-function validateISBN(isbn, elem) {
-	if(isbn.match(/[^0-9xX\.\-\s]/)) {
-		return false;
-	}
+						// This functions takes a string containing
+						// an ISBN (ISBN-10 or ISBN-13) and returns
+						// true if it's valid or false if it's invalid.
+						function validateISBN(isbn, elem) {
+						if(isbn.match(/[^0-9xX\.\-\s]/)) {
+						return false;
+						}
 
-	isbn = isbn.replace(/[^0-9xX]/g,'');
+						isbn = isbn.replace(/[^0-9xX]/g,'');
 
-	if(isbn.length != 10 && isbn.length != 13) {
-		return false;
-	}
-	if (isbn.length == 0) return true;
+						if(isbn.length != 10 && isbn.length != 13) {
+						return false;
+						}
+						if (isbn.length == 0) return true;
 
-	var ret;
-	var checkDigit = 0;
-	if(isbn.length == 10) {
-		checkDigit = 11 - ( (
-								 10 * isbn.charAt(0) +
-								 9  * isbn.charAt(1) +
-								 8  * isbn.charAt(2) +
-								 7  * isbn.charAt(3) +
-								 6  * isbn.charAt(4) +
-								 5  * isbn.charAt(5) +
-								 4  * isbn.charAt(6) +
-								 3  * isbn.charAt(7) +
-								 2  * isbn.charAt(8)
-								) % 11);
+						var ret;
+						var checkDigit = 0;
+						if(isbn.length == 10) {
+						checkDigit = 11 - ( (
+						10 * isbn.charAt(0) +
+						9  * isbn.charAt(1) +
+						8  * isbn.charAt(2) +
+						7  * isbn.charAt(3) +
+						6  * isbn.charAt(4) +
+						5  * isbn.charAt(5) +
+						4  * isbn.charAt(6) +
+						3  * isbn.charAt(7) +
+						2  * isbn.charAt(8)
+						) % 11);
 
-		if(checkDigit == 10) {
-			ret = (isbn.charAt(9) == 'x' || isbn.charAt(9) == 'X') ? true : false;
-		} else {
-			ret = (isbn.charAt(9) == checkDigit ? true : false);
-		}
-	} else {
-		checkDigit = 10 -  ((
-								 1 * isbn.charAt(0) +
-								 3 * isbn.charAt(1) +
-								 1 * isbn.charAt(2) +
-								 3 * isbn.charAt(3) +
-								 1 * isbn.charAt(4) +
-								 3 * isbn.charAt(5) +
-								 1 * isbn.charAt(6) +
-								 3 * isbn.charAt(7) +
-								 1 * isbn.charAt(8) +
-								 3 * isbn.charAt(9) +
-								 1 * isbn.charAt(10) +
-								 3 * isbn.charAt(11)
-								) % 10);
+						if(checkDigit == 10) {
+						ret = (isbn.charAt(9) == 'x' || isbn.charAt(9) == 'X') ? true : false;
+						} else {
+						ret = (isbn.charAt(9) == checkDigit ? true : false);
+						}
+						} else {
+						checkDigit = 10 -  ((
+						1 * isbn.charAt(0) +
+						3 * isbn.charAt(1) +
+						1 * isbn.charAt(2) +
+						3 * isbn.charAt(3) +
+						1 * isbn.charAt(4) +
+						3 * isbn.charAt(5) +
+						1 * isbn.charAt(6) +
+						3 * isbn.charAt(7) +
+						1 * isbn.charAt(8) +
+						3 * isbn.charAt(9) +
+						1 * isbn.charAt(10) +
+						3 * isbn.charAt(11)
+						) % 10);
 
-		if(checkDigit == 10) {
-			ret = (isbn.charAt(12) == 0 ? true : false) ;
-		} else {
-			ret = (isbn.charAt(12) == checkDigit ? true : false);
-		}
-	}
+						if(checkDigit == 10) {
+						ret = (isbn.charAt(12) == 0 ? true : false) ;
+						} else {
+						ret = (isbn.charAt(12) == checkDigit ? true : false);
+						}
+						}
 
-	elem = $(elem);
-	if (ret && isbn != elem.val() && elem.attr('id') != $(document.activeElement).attr('id'))
-		elem.val(isbn);
-	return ret;
-}
-</script>
+						elem = $(elem);
+						if (ret && isbn != elem.val() && elem.attr('id') != $(document.activeElement).attr('id'))
+						elem.val(isbn);
+						return ret;
+						}
+		</script>
--- application/views/pages/listbook.php
+++ application/views/pages/listbook.php
@@ -1,111 +1,111 @@
 <script type="text/javascript" charset="utf-8"> 
-var oTable;
+	var oTable;
 
-function fillDetails( nTr, aData ) {
-	nTr.addClass('details');
-	var cell = $('td', nTr);
-	$("<td></td>").insertBefore(cell);
-	cell.attr('colspan', cell.attr('colspan') - 1);
-	cell.append("<div class='center ui-state-active' style='width:100%'> Loading ... </div>");
-	
-	$.ajax({
-		url: '<?php echo URL::base() ?>json/getdetail/' + aData[1],
-		dataType: 'json',
-		success: function(data) {
-			cell.empty();
-			for (row in data) {
-				var detailRow = $('<div class="ui-state-active detail-box"></div>');
+	function fillDetails( nTr, aData ) {
+		nTr.addClass('details');
+		var cell = $('td', nTr);
+		$("<td></td>").insertBefore(cell);
+		cell.attr('colspan', cell.attr('colspan') - 1);
+		cell.append("<div class='center ui-state-active' style='width:100%'> Loading ... </div>");
 
-				var detailCode = $('<span class="detail-code"></span>');
-				detailCode.text(data[row].code + "-" + data[row].copy);
-				detailRow.append(detailCode);
+		$.ajax({
+			url: '<?php echo URL::base() ?>json/getdetail/' + aData[1],
+			dataType: 'json',
+			success: function(data) {
+				cell.empty();
+				for (row in data) {
+					var detailRow = $('<div class="ui-state-active detail-box"></div>');
 
-				var detailTitle = $('<span class="detail-title"></span>');
-				detailTitle.text(data[row].name );
-				detailRow.append(detailTitle);
+					var detailCode = $('<span class="detail-code"></span>');
+					detailCode.text(data[row].code + "-" + data[row].copy);
+					detailRow.append(detailCode);
 
-				var detailEdition = $('<span class="detail-edition"></span>');
-				detailEdition.text(data[row].version);
-				detailRow.append(detailEdition);
+					var detailTitle = $('<span class="detail-title"></span>');
+					detailTitle.text(data[row].name );
+					detailRow.append(detailTitle);
 
-				var detailAuthor = $('<span class="detail-author"></span>');
-				detailAuthor.text(data[row].author );
-				detailRow.append(detailAuthor);
+					var detailEdition = $('<span class="detail-edition"></span>');
+					detailEdition.text(data[row].version);
+					detailRow.append(detailEdition);
 
-				var detailPublisher = $('<span class="detail-publisher"></span>');
-				detailPublisher.text(data[row].publisher);
-				detailRow.append(detailPublisher);
+					var detailAuthor = $('<span class="detail-author"></span>');
+					detailAuthor.text(data[row].author );
+					detailRow.append(detailAuthor);
 
-				cell.append(detailRow);
+					var detailPublisher = $('<span class="detail-publisher"></span>');
+					detailPublisher.text(data[row].publisher);
+					detailRow.append(detailPublisher);
+
+					cell.append(detailRow);
+				}
+			},
+			error: function(req) {
+				cell.empty();
+				cell.append("<div class='center ui-state-error' style='width:100%'> Error! </div>");
 			}
-		},
-		error: function(req) {
-			cell.empty();
-			cell.append("<div class='center ui-state-error' style='width:100%'> Error! </div>");
-		}
-	});
-}
+		});
+	}
 
-$.fn.dataTableExt.oApi.fnSetFilteringDelay = function ( oSettings, iDelay ) {
-	/*
-	 * Type:        Plugin for DataTables (www.datatables.net) JQuery plugin.
-	 * Name:        dataTableExt.oApi.fnSetFilteringDelay
-	 * Version:     2.2.1
-	 * Description: Enables filtration delay for keeping the browser more
-	 *              responsive while searching for a longer keyword.
-	 * Inputs:      object:oSettings - dataTables settings object
-	 *              integer:iDelay - delay in miliseconds
-	 * Returns:     JQuery
-	 * Usage:       $('#example').dataTable().fnSetFilteringDelay(250);
-	 * Requires:	  DataTables 1.6.0+
-	 *
-	 * Author:      Zygimantas Berziunas (www.zygimantas.com) and Allan Jardine (v2)
-	 * Created:     7/3/2009
-	 * Language:    Javascript
-	 * License:     GPL v2 or BSD 3 point style
-	 * Contact:     zygimantas.berziunas /AT\ hotmail.com
-	 */
-	var
+	$.fn.dataTableExt.oApi.fnSetFilteringDelay = function ( oSettings, iDelay ) {
+		/*
+		* Type:        Plugin for DataTables (www.datatables.net) JQuery plugin.
+		* Name:        dataTableExt.oApi.fnSetFilteringDelay
+		* Version:     2.2.1
+		* Description: Enables filtration delay for keeping the browser more
+		*              responsive while searching for a longer keyword.
+		* Inputs:      object:oSettings - dataTables settings object
+		*              integer:iDelay - delay in miliseconds
+		* Returns:     JQuery
+		* Usage:       $('#example').dataTable().fnSetFilteringDelay(250);
+		* Requires:	  DataTables 1.6.0+
+		*
+		* Author:      Zygimantas Berziunas (www.zygimantas.com) and Allan Jardine (v2)
+		* Created:     7/3/2009
+		* Language:    Javascript
+		* License:     GPL v2 or BSD 3 point style
+		* Contact:     zygimantas.berziunas /AT\ hotmail.com
+		*/
+		var
 		_that = this,
 		iDelay = (typeof iDelay == 'undefined') ? 250 : iDelay;
-	
-	this.each( function ( i ) {
-		var
+
+		this.each( function ( i ) {
+			var
 			$this = this, 
 			oTimerId = null, 
 			sPreviousSearch = null,
 			sRealSearch = null;
 			anControl = $( 'input', _that.fnSettings().aanFeatures.f );
-		
-			anControl.unbind( 'keyup' ).bind( 'keyup', function(evt) {
-			var $$this = $this;
 
-			if (sPreviousSearch === null ||
+			anControl.unbind( 'keyup' ).bind( 'keyup', function(evt) {
+				var $$this = $this;
+
+				if (sPreviousSearch === null ||
 				sPreviousSearch != anControl.val() ||
 				evt.keyCode == 13 ||
-			    anControl.val() == '') {
-				window.clearTimeout(oTimerId);
-				sPreviousSearch = anControl.val();	
-				oTimerId = window.setTimeout(function() {
-					if (sRealSearch == null || sRealSearch != anControl.val()) {
-						sRealSearch = anControl.val();
-						_that.fnFilter( sRealSearch );
-					}
-				}, (evt.keyCode == 13 || anControl.val() == '') ? 0 : iDelay);
-			}
-		});
-		
-		return this;
-	} );
-	return this;
-}
+				anControl.val() == '') {
+					window.clearTimeout(oTimerId);
+					sPreviousSearch = anControl.val();	
+					oTimerId = window.setTimeout(function() {
+						if (sRealSearch == null || sRealSearch != anControl.val()) {
+							sRealSearch = anControl.val();
+							_that.fnFilter( sRealSearch );
+						}
+					}, (evt.keyCode == 13 || anControl.val() == '') ? 0 : iDelay);
+				}
+			});
 
-$(document).ready(function() {
-	$('#booksTable').dataTable( {
-		"aaSorting": [ [1, 'asc'] ],
-		"sDom": '<"H"lrf>t<"F"ip>',
-		"bAutoWidth" : true,
-		"fnRowCallback":
+			return this;
+		} );
+		return this;
+	}
+
+	$(document).ready(function() {
+		$('#booksTable').dataTable( {
+			"aaSorting": [ [1, 'asc'] ],
+			"sDom": '<"H"lrf>t<"F"ip>',
+			"bAutoWidth" : true,
+			"fnRowCallback":
 			function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) 
 			{
 				$('.btn-exp', nRow).click(function(evt) {
@@ -113,7 +113,7 @@ $(document).ready(function() {
 					if (src.hasClass('ui-icon-circlesmall-plus')) {
 						src.addClass('ui-icon-circlesmall-minus');
 						src.removeClass('ui-icon-circlesmall-plus');
-						
+
 						var aData = oTable.fnGetData( nRow );
 						var newRow = $(oTable.fnOpen( nRow, "", ""));
 						fillDetails(newRow, aData);
@@ -125,7 +125,7 @@ $(document).ready(function() {
 				});
 				return nRow;
 			},
-		"aoColumnDefs": [ 
+			"aoColumnDefs": [ 
 			{ 
 				"fnRender": function(id) {
 					return "<span class='ui-icon ui-icon-circlesmall-plus btn-exp'>[+]</span>";
@@ -147,30 +147,30 @@ $(document).ready(function() {
 				"sClass" : "col-code",
 				"aTargets" : [ 1 ]
 			}
-		],
-		"bInfo": true,
-		"bJQueryUI": true,
-		"bProcessing": true,
-		"bServerSide": true,
-		"sPaginationType": "full_numbers",
-		"sAjaxSource": '<?php echo URL::base() ?>json/list'
+			],
+			"bInfo": true,
+			"bJQueryUI": true,
+			"bProcessing": true,
+			"bServerSide": true,
+			"sPaginationType": "full_numbers",
+			"sAjaxSource": '<?php echo URL::base() ?>json/list'
+		} );
+		oTable = $('#booksTable').dataTable();
+		oTable.fnSetFilteringDelay(300);
 	} );
-	oTable = $('#booksTable').dataTable();
-	oTable.fnSetFilteringDelay(300);
-} );
 </script>
 <table id="booksTable"> 
-<thead> 
-<tr> 
-<th class="col-hdr-id"></th>
-<th class="col-hdr-code">Code</th> 
-<th class="col-hdr-name">Name</th> 
-<th class="col-hdr-author">author</th> 
-<th class="col-hdr-publisher">Publisher</th> 
-<th class="col-hdr-btn"></th>
-</tr> 
-</thead> 
-<tbody> 
-<tr></tr>
-</tbody> 
+	<thead> 
+		<tr> 
+			<th class="col-hdr-id"></th>
+			<th class="col-hdr-code">Code</th> 
+			<th class="col-hdr-name">Name</th> 
+			<th class="col-hdr-author">author</th> 
+			<th class="col-hdr-publisher">Publisher</th> 
+			<th class="col-hdr-btn"></th>
+		</tr> 
+	</thead> 
+	<tbody> 
+		<tr></tr>
+	</tbody> 
 </table> 
