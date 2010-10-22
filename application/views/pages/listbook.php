b0c3f527e05206220e7b7ade9591af827a6f8d35
Daniel Cheng Yuk-Pong <git@sdiz.net>
2010-10-22 14:33:29 +0800

    Use Template for code column

--- application/views/pages/listbook.php
+++ application/views/pages/listbook.php
@@ -14,11 +14,7 @@ Ext.CTYC.BookGrid = Ext.extend(Ext.grid.GridPanel,{
 			// grid columns
 			columns:[
 			{ id: 'id', header: 'id', dataIndex: 'id', sortable: true },
-			{ id: 'code', header: 'code', dataIndex: 'code', sortable: true,
-			renderer: function(v,m,r,x,y,s) {
-				return r.data['cat'] + r.data['code'] + '-' + r.data['copy'];
-			}
-			},
+			{ id: 'code', header: 'code', dataIndex: 'code', sortable: true, xtype:'templatecolumn', tpl: '{cat}{code}-{copy}' },
 				{ id: 'title', header: 'title', dataIndex: 'title', sortable: true },
 				{ id: 'author', header: 'author', dataIndex: 'author', sortable: true },
 				{ id: 'publisher', header: 'publisher', dataIndex: 'publisher', sortable: true },
@@ -77,8 +73,6 @@ Ext.onReady(function(){
 									if (e.keyCode == 13) {
 										alert(o.getValue());
 									}
-e.stopPropagation();
-return false;
 								}
 							},
 							scope: this
