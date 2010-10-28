e10589b1fcf86c6ad60815eeeea24f830b4b5885
Daniel Cheng Yuk-Pong <git@sdiz.net>
2010-10-28 16:22:31 +0800

    More fields in edit, fix style(?)

--- static/js/bookedit.js
+++ static/js/bookedit.js
@@ -6,31 +6,40 @@ function handleBookEdit(grid, rowIndex, colIndex) {
 	var store = grid.getStore();
 	var rec = store.getAt(rowIndex);
 
-	var form = new Ext.form.FormPanel(
+	var formPanel = new Ext.form.FormPanel(
 	{
-		labelWidth: 55,
+		baseCls: 'x-plain',
+		labelWidth: 85,
 		defaultType: 'textfield',
 		items: [
-			{ fieldLabel: 'Label 1', name: 'title', anchor:'100%'},
-			{ fieldLabel: 'Label 2', name: 'author', anchor:'100%'}
+			{ fieldLabel: 'Code', name: 'code', anchor:'100%', xtype: 'displayfield'},
+			{ fieldLabel: 'Title', name: 'title', anchor:'100%'},
+			{ fieldLabel: 'Author', name: 'author', anchor:'100%'},
+			{ fieldLabel: 'Publisher', name: 'publisher', anchor:'100%'},
+			{ fieldLabel: 'Publish Date', name: 'publishDate', xtype: 'datefield', format: 'Y-m-d'},
+			{ fieldLabel: 'Edition', name: 'edition', anchor:'100%'},
+			{ fieldLabel: 'ISBN', name: 'isbn', anchor:'100%'}
 		]
 	});
-	form.getForm().loadRecord(rec);
+	var form = formPanel.getForm();
+	form.loadRecord(rec);
+	form.findField('code').setValue(rec.data['cat'] + rec.data['code'] + '-' + rec.data['copy']);
 
 	var win = new Ext.Window({
+		plain: true,
 		bodyStyle: 'padding:5px',
 		animateTarget: row,
 		modal: true,
 		layout: "fit",
 		width: 500,
-		height: 400,
+		height: 350,
 		minWidth: 300,
-		minHeight: 200,
+		minHeight: 350,
 		title: "Title - " + rec.data.title,
-		items: form,
+		items: formPanel,
 buttons: [  { text: 'Save', iconCls: 'icon-save', handler: function() {
-form.getForm().updateRecord(rec);
-form.ownerCt.close();
+form.updateRecord(rec);
+formPanel.ownerCt.close();
 } } ]
 	});
 	win.show();
